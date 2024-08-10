b43e4d7f0bb9c8aab9e39afd912afeb863dd0ad4
Vishal Singh <vishal.kr.singh2021@gmail.com>
2024-08-10 20:14:29 +0530

    Vishal|Added the complete rest part of code for ecommerce application

--- src/main/java/com/ecommerce/project/model/Category.java
+++ src/main/java/com/ecommerce/project/model/Category.java
@@ -1,31 +1,27 @@
 package com.ecommerce.project.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-//if we addlike (name="") then in that case we are defining that it should be having certain name
-// than the preent class name
+import java.util.List;
 
 @Entity(name = "categories")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class Category {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    Long categoryId;
+    private Long categoryId;
 
     @NotBlank
-    @Size(min=3,max=10, message="Category should be within the range 3 to 10 characters")
-    String category;
+    @Size(min = 5, message = "Category name must contain atleast 5 characters")
+    private String categoryName;
 
+    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL)
+    private List<Product> products;
 }
--- src/main/java/com/ecommerce/project/repository/CategoryRepository.java
+++
@@ -1,7 +0,0 @@
-package com.ecommerce.project.repository;
-
-import com.ecommerce.project.model.Category;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CategoryRepository extends JpaRepository<Category,Long> {
-}
